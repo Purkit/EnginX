7e67be70bfe73b16879067ed27922791e1051619
Mriganka Purkait <mrigankapurkait64@gmail.com>
2023-09-07 19:04:43 +0530

    Added platform initialization interface.

---
+++ enginx/include/enginx/platform/init.hpp
@@ -0,0 +1,15 @@
+#ifndef PLATFORM_INITIALIZATION_HPP
+#define PLATFORM_INITIALIZATION_HPP
+
+namespace enginx
+{
+    class Platform
+    {
+        public:
+            static void StartUp();
+            static void Shutdown();
+
+    };
+}
+
+#endif // ! PLATFORM_INITIALIZATION_HPP
